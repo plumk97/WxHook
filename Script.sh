9bd5b31cbcf506b805f0e143d16a1c4f51715925
AQY <AQY@AQYdeiMac.local>
2018-03-26 12:00:40 +0800

    s

--- Script.sh
+++ Script.sh
@@ -8,3 +8,6 @@
 
 rm -rf ~/Documents/inject/wx/Payload/WeChat.app/WxHook.dylib
 cp `pwd`/LatestBuild/Debug-iphoneos/WxHook.dylib ~/Documents/inject/wx/Payload/WeChat.app/WxHook.dylib
+
+rm -rf ~/Documents/inject/wx/Payload/WeChat.app/RevealServer.framework
+cp -r `pwd`/RevealServer.framework ~/Documents/inject/wx/Payload/WeChat.app/RevealServer.framework
